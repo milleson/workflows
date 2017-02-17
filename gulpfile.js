9bc8ddc088ac93478c6d845429a38a78847e04df
Paul Milleson <paul_milleson@yahoo.com>
2017-02-16 18:32:00 -0800

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
-
-gulp.task('log', function() {
-    gutil.log('Workflows are awesome');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
+var coffeeSources = ['components/coffee/*.coffee'];
+gulp.task('coffee', function() {
+    gulp.src(coffeeSources)
+        .pipe(coffee({ bare: true })
+            .on('error', gutil.log)) 
+        .pipe(gulp.dest('components/scripts'))
 });
